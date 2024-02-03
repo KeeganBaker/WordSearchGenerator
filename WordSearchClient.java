b9f97706d21723b0973c97f900723d8fd093e40b
Keegan Baker <keeganbaker1802@gmail.com>
2024-02-02 19:38:20 -0800

    Initial Commit

---
+++ WordSearchClient.java
@@ -0,0 +1,24 @@
+// Name: Keegan Baker
+// Date: 
+// Project: CS 145 
+// Purpose: 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+public class WordSearchClient {
+    public static void main(String[] args) throws Exception {
+    
+    // command list
+    System.out.println
+    ("=================================================================");
+    System.out.println("Welcome to the word search generator!");
+    System.out.println("This program allows you to create a word search puzzle.\n");
+    System.out.println("Enter a command to start:");
+    System.out.println("p: to print a string representation of the inventory.");
+    System.out.println("a: to create another string and add their representations.");
+    System.out.println("e: to set the value of a specific letter");
+    System.out.println("q: to quit the program");
+    System.out.println
+    ("=================================================================");
+    }
+}
